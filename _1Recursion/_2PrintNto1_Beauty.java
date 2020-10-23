9f8f844494ccc7c45915f870a3b30361e23d42c4
mohammed.audhil <mohammed.audhil@zohocorp.com>
2020-10-24 01:15:42 +0530

    add heightOfTree using recursion

--- _1Recursion/_2PrintNto1_Beauty.java
+++ _1Recursion/_2PrintNto1_Beauty.java
@@ -17,9 +17,30 @@ public class _2PrintNto1_Beauty {
         print(n - 1);
     }
 
+    int n1 = 0, n2 = 1, n3 = 0;
+
+    private void printFib(int n) {
+        if (n > 0) {
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+            System.out.println(n3);
+            printFib(n - 1);
+        }
+    }
+
+    private int fact(int n) {
+        if (n == 0)
+            return 1;
+        else
+            return n * fact(n - 1);
+    }
+
     public static void main(String[] args) {
         _2PrintNto1_Beauty prob = new _2PrintNto1_Beauty();
-        System.out.println("yup print N to 1");
-        prob.print(8);
+//        System.out.println("yup print N to 1");
+//        prob.print(8);
+//        prob.printFib(8);
+        System.out.println("yup:  factorial: " + prob.fact(5));
     }
 }
