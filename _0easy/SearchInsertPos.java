73662b80878097d2d2672abebadd329d2c2c1276
Audhil <smdaudhilbe@gmail.com>
2021-06-03 21:11:42 +0530

    add SearchInsertPos

---
+++ _0easy/SearchInsertPos.java
@@ -0,0 +1,59 @@
+package _0easy;
+
+//  https://leetcode.com/problems/search-insert-position/
+/*
+* Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+Example 4:
+
+Input: nums = [1,3,5,6], target = 0
+Output: 0
+Example 5:
+
+Input: nums = [1], target = 0
+Output: 0
+* */
+public class SearchInsertPos {
+
+    public static int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0)
+            return 0;
+
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+        }
+        return nums[left] < target ? left + 1 : left;
+    }
+
+    public static void main(String[] args) {
+//        System.out.println("yup: " + searchInsert(new int[]{1, 3, 5, 6}, 5));
+//        System.out.println("yup: " + searchInsert(new int[]{1, 3, 5, 6}, 2));
+//        System.out.println("yup: " + searchInsert(new int[]{1}, 0));
+        System.out.println("yup: " + searchInsert(new int[]{1, 3, 5, 6}, -2));
+
+    }
+}
