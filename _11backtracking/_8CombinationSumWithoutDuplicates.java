8f4f6ca2843d868273c638879ec3bb19081c4e64
Audhil <smdaudhilbe@gmail.com>
2021-06-23 21:25:06 +0530

    add _8CombinationSumWithoutDuplicates prob

--- _11backtracking/_8CombinationSumWithoutDuplicates.java
+++ _11backtracking/_8CombinationSumWithoutDuplicates.java
@@ -1,20 +1,20 @@
 package _11backtracking;
 
+//  https://www.youtube.com/watch?v=bh3dXVsRHsc&list=PLNxqWc8Uj2LTaaxs-8vzK0Ft47rMggFnN&index=11
+
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-//  https://www.youtube.com/watch?v=KvxUS8QlLfY&list=PLNxqWc8Uj2LTaaxs-8vzK0Ft47rMggFnN&index=10
-//  TC: O(2 ^ (min(target/minElement ,  n)))
-//  notes @ https://photos.google.com/album/AF1QipMLL_acuhN6YogZ1XMenF02iNt7vVctR7ZvlyE_/photo/AF1QipPlD2FNS7-wQEfJ0Hn37hbBVs-eWwMRlxaM2a_w
-public class _7CombinationSumWithDuplicates {
-
+public class _8CombinationSumWithoutDuplicates {
     private static void cbWithDuplicates(int i,
                                          int n,
                                          int[] arr,
                                          int sum,
                                          int target,
-                                         List<Integer> currCombination,
-                                         List<List<Integer>> result) {
+                                         List<Integer> currCombination,  //  using set to avoid duplicates
+                                         Set<List<Integer>> result) {
         if (sum == target) {
             result.add(new ArrayList<>(currCombination));
             return;
@@ -23,10 +23,10 @@ public class _7CombinationSumWithDuplicates {
             return;
         }
 
-        //  include ith element - iterating left side
+        //  include ith element, iterating left side
         sum += arr[i];
         currCombination.add(arr[i]);
-        cbWithDuplicates(i, n, arr, sum, target, currCombination, result);
+        cbWithDuplicates(i + 1, n, arr, sum, target, currCombination, result);
 
         //  exclude ith element, move to next iteration - right side
         sum -= arr[i];
@@ -35,21 +35,20 @@ public class _7CombinationSumWithDuplicates {
     }
 
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3};  //  always sort the order, if need - to get end result sorted
+        int[] arr = {1, 1, 2, 3};  //  always sort the order, if need - to get end result sorted
         int target = 4;
-        List<List<Integer>> result = new ArrayList<>();
+        Set<List<Integer>> result = new HashSet<>();
         cbWithDuplicates(0, arr.length, arr, 0, target, new ArrayList<>(), result);
-        System.out.println("yup: the combinations are");
+        System.out.println("yup: the combinations without duplicates are");
         for (List<Integer> combination : result) {
             System.out.println(combination);
         }
         /*
         * o/p
         * yup: the combinations are
-            [1, 1, 1, 1]
-            [1, 1, 2]
-            [1, 3]
-            [2, 2]
+            yup: the combinations without duplicates are
+                [1, 1, 2] -> note, both 1's are different, we've 1 twice in question
+                [1, 3]
         */
     }
 }
