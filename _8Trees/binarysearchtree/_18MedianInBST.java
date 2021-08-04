ee241dee8ffa9cd107e829f7f674bdd22016616d
Audhil <smdaudhilbe@gmail.com>
2021-08-05 00:00:42 +0530

    add _19MorrisTraversal prob

---
+++ _8Trees/binarysearchtree/_18MedianInBST.java
@@ -0,0 +1,14 @@
+package _8Trees.binarysearchtree;
+
+//  https://www.youtube.com/watch?v=CVXPIRMjO-w&list=PLNxqWc8Uj2LQpbGv6I_HWf0kTj6kMVVwm&index=20
+public class _18MedianInBST {
+
+    static class TreeNode {
+        int value;
+        TreeNode left, right;
+
+        public TreeNode(int value) {
+            this.value = value;
+        }
+    }
+}
