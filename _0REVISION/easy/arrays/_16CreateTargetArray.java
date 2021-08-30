d210e42bb264aed77049f31ecc4f2dabf824342c
Audhil <smdaudhilbe@gmail.com>
2021-08-30 21:58:02 +0530

    add _16CreateTargetArray, _17CountItemMatching probs

---
+++ _0REVISION/easy/arrays/_16CreateTargetArray.java
@@ -0,0 +1,26 @@
+package _0REVISION.easy.arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class _16CreateTargetArray {
+
+    public static int[] createTargetArray(int[] nums, int[] index) {
+        if (nums == null || index == null)
+            return null;
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < index.length; i++) {
+            list.add(index[i], nums[i]);
+        }
+        for (int i = 0; i < list.size(); i++) {
+            nums[i] = list.get(i);
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("yup: _16CreateTargetArray: " + Arrays.toString(createTargetArray(new int[]{0, 1, 2, 3, 4}, new int[]{0, 1, 2, 2, 1})));
+    }
+}
