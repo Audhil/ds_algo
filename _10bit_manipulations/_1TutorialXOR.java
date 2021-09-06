4d5fcd351f3a1aee244a7fe054b1851f796e30ae
Audhil <smdaudhilbe@gmail.com>
2021-09-06 22:40:47 +0530

    add bitwise operations & probs

---
+++ _10bit_manipulations/_1TutorialXOR.java
@@ -0,0 +1,51 @@
+package _10bit_manipulations;
+
+public class _1TutorialXOR {
+
+    /*
+     * XOR - https://www.youtube.com/watch?v=bTauscvOymA
+     *
+     * 1. 5 ^ 5 = 0
+     * 2. 0 ^ 5 = 5
+     * */
+    /*
+      1. find no that is not repeeated in the array
+    * */
+    //  TC: O(n) & SC: O(1)
+    private static int findNonRepeatedNumber(int[] arr) {
+        int res = 0;
+        for (int j : arr) {
+            res ^= j;
+        }
+        return res;
+    }
+
+    /*
+     * 2. find 2 Non repeated items in the array
+     * TC: O(n) & SC: O(1)
+     * */
+    private static void find2NonRepeatedNumbers(int[] arr) {
+        int res = 0;
+        for (int j : arr) {
+            res ^= j;
+        }
+        //  res will have 2 unique numbers XORed together
+        //  find bit whose last bit is set
+//        res &= -res;
+        res &= ~(res - 1);
+        //  iterate through array and divide it in to 2 parts(one with items ending with 1, another ending with 0)
+        int sum1 = 0, sum2 = 0;
+        for (int j : arr) {
+            if ((j & res) > 0)
+                sum1 ^= j;
+            else
+                sum2 ^= j;
+        }
+        System.out.println("yup: sum1: " + sum1 + ", sum2: " + sum2 + " are non repeating numbers");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("yup: non repeated item: " + findNonRepeatedNumber(new int[]{5, 3, 5, 1, 6, 7, 6, 1, 7}));   //  yup: non repeated item: 3
+        find2NonRepeatedNumbers(new int[]{2, 3, 7, 9, 11, 2, 3, 11});
+    }
+}
---
+++ _10bit_manipulations/_medium/_0ReverseBits.java
@@ -0,0 +1,28 @@
+package _10bit_manipulations._medium;
+
+//  https://www.interviewbit.com/problems/reverse-bits/
+/*
+* Problem Description
+
+Reverse the bits of an 32 bit unsigned integer A.
+
+
+
+Problem Constraints
+0 <= A <= 2^32
+
+
+
+Input Format
+First and only argument of input contains an integer A.
+
+
+
+Output Format
+Return a single unsigned integer denoting the decimal value of reversed bits.*/
+public class _0ReverseBits {
+
+    public long reverse(long a) {
+        return 0;
+    }
+}
