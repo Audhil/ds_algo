797bc5537493fe39af8ed023d72599a43e972906
Audhil <smdaudhilbe@gmail.com>
2021-09-29 22:25:38 +0530

    add _0ReverseBits prob

--- _10bit_manipulations/_medium/_0ReverseBits.java
+++ _10bit_manipulations/_medium/_0ReverseBits.java
@@ -34,7 +34,36 @@ public class _0ReverseBits {
         return res;
     }
 
+    //  logic is correct, but leading zeros are making issues
+    public static int reverseWithBitwise(int n) {
+        int mask = 0b11111111111111110000000000000000;
+        int temp = n & mask;
+        temp >>>= 16;
+        int mask2 = 0b00000000000000001111111111111111;
+        int temp2 = n & mask2;
+        temp2 <<= 16;
+        int mask3 = 0b1111111100000000;
+        int t = temp & mask3;
+        t >>>= 8;
+        int mask4 = 0b0000000011111111;
+        int t2 = temp & mask4;
+        t2 <<= 8;
+        temp = t | t2;
+
+        int t3 = temp2 & mask3;
+        t3 >>>= 8;
+        int t4 = temp2 & mask4;
+        t4 <<= 8;
+        temp2 = t3 | t4;
+
+        int res = temp | temp2;
+        System.out.println("yup: Integer.toBinaryString(res): " + Integer.toBinaryString(res));
+        return res;
+    }
+
     public static void main(String[] args) {
-        System.out.println("yup: " + reverse(0b0011));
+//        System.out.println("yup: reverse: " + reverse(0b0011));
+        System.out.println("yup: reverseWithBitwise: " + reverseWithBitwise(0b10101010101010100000000000000111));
+//        System.out.println("yup: reverseWithBitwise: " + reverseWithBitwise(0b11111111111111110000000000000000));
     }
 }
