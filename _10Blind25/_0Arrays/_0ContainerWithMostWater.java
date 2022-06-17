b282a293b20d24af8c9c86197c8a81d223a2efb8
Audhil <smdaudhilbe@gmail.com>
2022-06-17 21:48:01 +0530

    add _0ContainerWithMostWater optimized approach

--- _10Blind25/_0Arrays/_0ContainerWithMostWater.java
+++ _10Blind25/_0Arrays/_0ContainerWithMostWater.java
@@ -12,13 +12,37 @@ public class _0ContainerWithMostWater {
     int area = 0;
     for (int left = 0; left < height.length; left++) {
       for (int right = left + 1; right < height.length; right++) {
-        area = Math.max(area, (right - left) * (Math.min(height[left], height[right])));
+        area = Math.max(area, (Math.min(height[left], height[right])) * (right - left)); // l * b
       }
     }
     return area;
   }
 
+  //  2 pointer approach - O (n)
+  public static int maxAreaWith2Pointer(int[] height) {
+    if (height == null) {
+      return 0;
+    }
+    int area = 0;
+    int left = 0, right = height.length - 1;
+    while (left < right) {
+      area = Math.max(area, (right - left) * (Math.min(height[left], height[right])));
+      if (height[left] < height[right]) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+    return area;
+  }
+
+
   public static void main(String[] args) {
-    System.out.println("yup: maxArea: " + maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
+    System.out.println(
+        "yup: maxArea: " + maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7})); //  yup: maxArea: 49
+
+    System.out.println(
+        "yup: maxAreaWith2Pointer: " + maxAreaWith2Pointer(
+            new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7})); //  yup: maxAreaWith2Pointer: 49
   }
 }
