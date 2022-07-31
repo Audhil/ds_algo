ae6411da38f3e364e35aadc8def2d842f667a0b3
Audhil <smdaudhilbe@gmail.com>
2022-08-01 00:00:13 +0530

    add _13LCAofBST_E prob

--- _10Blind75/_7Trees/_12KthSmallestElementInBST_M.java
+++ _10Blind75/_7Trees/_12KthSmallestElementInBST_M.java
@@ -1,5 +1,7 @@
 package _10Blind75._7Trees;
 
+import java.util.Stack;
+
 //  https://leetcode.com/problems/kth-smallest-element-in-a-bst/
 //  refer: _11KthSmallestElementInBST.java  - for recursive
 //  https://www.youtube.com/watch?v=5LUXSvjmGCw&ab_channel=NeetCode - for iterative
@@ -37,6 +39,26 @@ public class _12KthSmallestElementInBST_M {
     dfs(root.right, res, kArr);
   }
 
+  //  iterative
+  private static int kthSmallestIterative(TreeNode root, int k) {
+    int n = 0;
+    Stack<TreeNode> stack = new Stack<>();
+    TreeNode cur = root;
+    while (cur != null) {
+      while (cur != null) {
+        stack.push(cur);
+        cur = cur.left;
+      }
+      cur = stack.pop();
+      n++;
+      if (n == k) {
+        return cur.data;
+      }
+      cur = cur.right;
+    }
+    return -1;
+  }
+
   public static void main(String[] args) {
     TreeNode root = new TreeNode(3);
     root.left = new TreeNode(1);
@@ -49,6 +71,9 @@ public class _12KthSmallestElementInBST_M {
     root2.left.left = new TreeNode(2);
     root2.left.right = new TreeNode(4);
     root2.left.left.left = new TreeNode(1);
-    System.out.println("yup: 2nd kth smallest item: " + kthSmallest(root, 3));  //  3
+    System.out.println("yup: 2nd kth smallest item: " + kthSmallest(root2, 3));  //  3
+    System.out.println("yup: iterative results:");
+    System.out.println("yup: kth iterative smallest item: " + kthSmallestIterative(root, 1));  //  1
+    System.out.println("yup: kth iterative  smallest item: " + kthSmallestIterative(root2, 3));  //  -1 //  this is giving wrong answer
   }
 }
