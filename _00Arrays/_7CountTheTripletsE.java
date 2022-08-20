f4916c321afb480774132ae6642c51d14f4f7094
Audhil <smdaudhilbe@gmail.com>
2022-08-20 21:04:13 +0530

    add countTriplet function

---
+++ _00Arrays/_7CountTheTripletsE.java
@@ -0,0 +1,29 @@
+package _00Arrays;
+
+//  https://www.youtube.com/watch?v=eZJvV3bmEsA&ab_channel=KrishnaTeaches
+//  https://practice.geeksforgeeks.org/problems/count-the-triplets4615/1
+public class _7CountTheTripletsE {
+
+  private static int countTriplet(int[] arr, int n) {
+    //  1. item available arr
+    int[] itemAvailableArr = new int[100000]; //  constraint given in arr
+    for (int item : arr) {
+      itemAvailableArr[item] = 1;
+    }
+    //  2. check for the triplets
+    int cnt = 0;
+    for (int i = 0; i < n - 1; i++) {
+      for (int j = i + 1; j < n; j++) {
+        if (itemAvailableArr[arr[i] + arr[j]] == 1) {
+          cnt++;
+        }
+      }
+    }
+    return cnt;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("yup: countTriplet: " + countTriplet(new int[]{1, 5, 3, 2}, 4)); //  2
+    System.out.println("yup: countTriplet: " + countTriplet(new int[]{2, 3, 4}, 3));  //  0
+  }
+}
---
+++ _00Arrays/_8Merge2ArraysH.java
@@ -0,0 +1,13 @@
+package _00Arrays;
+
+//  https://www.youtube.com/watch?v=3NV9z6prFGY&ab_channel=KrishnaTeaches
+public class _8Merge2ArraysH {
+
+  private static int[] merge2Arrays(int[] arr1, int[] arr2) {
+    int p1 = 0, p2 = 0;
+    while (p1 < arr1.length && p2 < arr2.length) {
+
+    }
+    return arr1;
+  }
+}
