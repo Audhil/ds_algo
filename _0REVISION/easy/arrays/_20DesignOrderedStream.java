8ce5d46b17aab2a60ac97c7e90215b8668d720c2
Audhil <smdaudhilbe@gmail.com>
2022-09-02 19:40:23 +0530

    add revision to _20DesignOrderedStream class

--- _0REVISION/easy/arrays/_20DesignOrderedStream.java
+++ _0REVISION/easy/arrays/_20DesignOrderedStream.java
@@ -6,31 +6,40 @@ import java.util.List;
 //  https://leetcode.com/problems/design-an-ordered-stream/
 public class _20DesignOrderedStream {
 
-    private static class OrderedStream {
+  private static class OrderedStream {
 
-        private String[] arr;
-        private int ptr = 0;
+    private final String[] arr;
+    private int ptr = 0;
 
-        public OrderedStream(int n) {
-            arr = new String[n];
-        }
-
-        public List<String> insert(int idKey, String value) {
-            List<String> list = new ArrayList<>();
-            arr[idKey - 1] = value;
-            while (ptr < arr.length && arr[ptr] != null) {
-                list.add(arr[ptr++]);
-            }
-            return list;
-        }
+    public OrderedStream(int n) {
+      arr = new String[n];
     }
 
-    public static void main(String[] args) {
-        OrderedStream orderedStream = new OrderedStream(5);
-        System.out.println(orderedStream.insert(3, "ccccc"));
-        System.out.println(orderedStream.insert(1, "aaaaa"));
-        System.out.println(orderedStream.insert(2, "bbbbb"));
-        System.out.println(orderedStream.insert(5, "eeeee"));
-        System.out.println(orderedStream.insert(4, "ddddd"));
+    public List<String> insert(int idKey, String value) {
+      List<String> list = new ArrayList<>();
+      arr[idKey - 1] = value;
+      while (ptr < arr.length && arr[ptr] != null) {
+        list.add(arr[ptr++]);
+      }
+      return list;
     }
+
+    public List<String> revision(int idKey, String value) {
+      List<String> resList = new ArrayList<>();
+      arr[idKey - 1] = value;
+      while (ptr < arr.length && arr[ptr] != null) {
+        resList.add(arr[ptr++]);
+      }
+      return resList;
+    }
+  }
+
+  public static void main(String[] args) {
+    OrderedStream orderedStream = new OrderedStream(5);
+    System.out.println(orderedStream.insert(3, "ccccc"));   //  []
+    System.out.println(orderedStream.insert(1, "aaaaa"));   //  [aaaaa]
+    System.out.println(orderedStream.insert(2, "bbbbb"));   //  [bbbbb, ccccc]
+    System.out.println(orderedStream.insert(5, "eeeee"));   //  []
+    System.out.println(orderedStream.insert(4, "ddddd"));   //  [ddddd, eeeee]
+  }
 }
