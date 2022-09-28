58e1456cbac188d8d40fd3a8bdaad026def1f755
Audhil <smdaudhilbe@gmail.com>
2022-09-28 20:43:36 +0530

    add _9Pick3Sticks_Equilateral_Triangle_E prob

---
+++ _0dynamic_programming/_neetcode/__2dDP/_3CoinChangeII_NoOfWaysToChoose_M.java
@@ -0,0 +1,9 @@
+package _0dynamic_programming._neetcode.__2dDP;
+
+//  https://youtu.be/qMky6D6YtXU?t=2704
+public class _3CoinChangeII_NoOfWaysToChoose_M {
+
+  //  TC: O (c^n) -> we can reduce it to O (c * n) & SC: O (c * n)
+  //  dp solution - TC: O (c^n), SC: (n)  - https://youtu.be/qMky6D6YtXU?t=3178
+
+}
