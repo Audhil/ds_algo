edf1af1bb820ec991f234495820a5f853e1ce75e
Audhil <smdaudhilbe@gmail.com>
2022-10-03 12:23:47 +0530

    add sudoko, valid matrix probs

---
+++ _00Arrays/_12ValidSudokoM.java
@@ -0,0 +1,69 @@
+package _00Arrays;
+
+import java.util.HashSet;
+import java.util.Set;
+
+//  https://youtu.be/TjFXEUCMqI8
+//  https://leetcode.com/problems/valid-sudoku/
+public class _12ValidSudokoM {
+
+  public static boolean isValidSudoku(char[][] board) {
+    Set<String> hs = new HashSet<>();
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board[0].length; j++) {
+        if (board[i][j] != '.') {
+          String r = 'r' + "-" + i + "-" + board[i][j];
+          String c = 'c' + "-" + j + "-" + board[i][j];
+//          String rc = (i / 3) + "-" + board[i][j] + "-" + (j / 3);
+          String rc = (i / 3) + "-" + (j / 3) + "-" + board[i][j];
+          //  Returns: true if this set did not already contain the specified element
+          if (!hs.add(r) || !hs.add(c) || !hs.add(rc)) {
+            return false;
+          }
+        }
+      }
+    }
+    return true;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("yup: is valid sudoko: " +
+        isValidSudoku(new char[][]{
+            {'5', '3', '.', '.', '7', '.', '.', '.', '.'}
+            , {'6', '.', '.', '1', '9', '5', '.', '.', '.'}
+            , {'.', '9', '8', '.', '.', '.', '.', '6', '.'}
+            , {'8', '.', '.', '.', '6', '.', '.', '.', '3'}
+            , {'4', '.', '.', '8', '.', '3', '.', '.', '1'}
+            , {'7', '.', '.', '.', '2', '.', '.', '.', '6'}
+            , {'.', '6', '.', '.', '.', '.', '2', '8', '.'}
+            , {'.', '.', '.', '4', '1', '9', '.', '.', '5'}
+            , {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        }));
+
+    System.out.println("yup: is valid sudoko: " +
+        isValidSudoku(new char[][]{
+            {'8', '3', '.', '.', '7', '.', '.', '.', '.'}
+            , {'6', '.', '.', '1', '9', '5', '.', '.', '.'}
+            , {'.', '9', '8', '.', '.', '.', '.', '6', '.'}
+            , {'8', '.', '.', '.', '6', '.', '.', '.', '3'}
+            , {'4', '.', '.', '8', '.', '3', '.', '.', '1'}
+            , {'7', '.', '.', '.', '2', '.', '.', '.', '6'}
+            , {'.', '6', '.', '.', '.', '.', '2', '8', '.'}
+            , {'.', '.', '.', '4', '1', '9', '.', '.', '5'}
+            , {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        }));
+
+    System.out.println("yup: is valid sudoko: " +
+        isValidSudoku(new char[][]{
+            {'.', '.', '.', '.', '5', '.', '.', '1', '.'},
+            {'.', '4', '.', '3', '.', '.', '.', '.', '.'},
+            {'.', '.', '.', '.', '.', '3', '.', '.', '1'},
+            {'8', '.', '.', '.', '.', '.', '.', '2', '.'},
+            {'.', '.', '2', '.', '7', '.', '.', '.', '.'},
+            {'.', '1', '5', '.', '.', '.', '.', '.', '.'},
+            {'.', '.', '.', '.', '.', '2', '.', '.', '.'},
+            {'.', '2', '.', '9', '.', '.', '.', '.', '.'},
+            {'.', '.', '4', '.', '.', '.', '.', '.', '.'}
+        }));
+  }
+}
---
+++ _0easy/CheckValidMatrixE.java
@@ -0,0 +1,76 @@
+package _0easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+//  https://leetcode.com/problems/check-if-every-row-and-column-contains-all-numbers/
+//  https://youtu.be/rOZgz6gc8Yo
+public class CheckValidMatrixE {
+
+  //  TC: O (n^2); SC: O (n^2)
+  public static boolean checkValid(int[][] matrix) {
+    int n = matrix.length;
+    Set<Integer> set;
+    //  move row wise
+    for (int i = 0; i < n; i++) {
+      set = new HashSet<>();
+      for (int j = 0; j < n; j++) {
+        if (!set.add(matrix[i][j])) {
+          return false;
+        }
+      }
+    }
+    //  move col wise
+    for (int i = 0; i < n; i++) {
+      set = new HashSet<>();
+      for (int j = 0; j < n; j++) {
+        if (!set.add(matrix[j][i])) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  //  using XOR - a^a = 0; 0^a = a;
+  //  TC: O (n^2); SC: O (1)
+  public static boolean checkValidWithXor(int[][] matrix) {
+    int n = matrix.length;
+    int rXor, cXor;
+    for (int i = 0; i < n; i++) {
+      rXor = 0;
+      cXor = 0;
+      for (int j = 0; j < n; j++) {
+        //  0^(value at 0,0)^1; 0^(value at 0,1)^2; 0^(value at 0,2)^3
+        rXor ^= matrix[i][j] ^ (j + 1);
+        cXor ^= matrix[j][i] ^ (j + 1);
+      }
+      if (rXor != 0 || cXor != 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static void main(String[] args) {
+//    System.out.println("yup: is valid matrix: " + checkValid(new int[][]{
+//        {1, 1, 1}, {1, 2, 3}, {1, 2, 3}
+//    }));  //  yup: is valid matrix: false
+//    System.out.println(
+//        "yup: is valid matrix with checkValidWithXor: " + checkValidWithXor(new int[][]{
+//            {1, 1, 1}, {1, 2, 3}, {1, 2, 3}
+//        }));  //  yup: is valid matrix: false
+//    System.out.println(
+//        "yup: is valid matrix with checkValid: " + checkValid(new int[][]{
+//            {1, 2, 2, 4, 5, 6, 6}, {2, 2, 4, 5, 6, 6, 1}, {2, 4, 5, 6, 6, 1, 2},
+//            {4, 5, 6, 6, 1, 2, 2}, {5, 6, 6, 1, 2, 2, 4}, {6, 6, 1, 2, 2, 4, 5},
+//            {6, 1, 2, 2, 4, 5, 6}
+//        }));  //  yup: is valid matrix: false
+    System.out.println(
+        "yup: is valid matrix with checkValidWithXor: " + checkValidWithXor(new int[][]{
+            {1, 2, 2, 4, 5, 6, 6}, {2, 2, 4, 5, 6, 6, 1}, {2, 4, 5, 6, 6, 1, 2},
+            {4, 5, 6, 6, 1, 2, 2}, {5, 6, 6, 1, 2, 2, 4}, {6, 6, 1, 2, 2, 4, 5},
+            {6, 1, 2, 2, 4, 5, 6}
+        }));  //  yup: is valid matrix: false
+  }
+}
