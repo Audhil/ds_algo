afd49f956559c1293546002bb68ef52327bcf311
Audhil <smdaudhilbe@gmail.com>
2022-12-04 16:08:14 +0530

    add revision

--- _10Blind75/_7Trees/_11BalancedBinaryTreeE.java
+++ _10Blind75/_7Trees/_11BalancedBinaryTreeE.java
@@ -5,6 +5,9 @@ package _10Blind75._7Trees;
 //  trick is to check the balance condition from leaves
 public class _11BalancedBinaryTreeE {
 
+  //  Given a binary tree, determine if it is height-balanced (A height-balanced binary tree is a binary tree
+  //  in which the depth of the two subtrees of every node never differs by more than one.)
+
   private static class TreeNode {
 
     int val;
@@ -44,6 +47,21 @@ public class _11BalancedBinaryTreeE {
     return new Pair(isBalanced, 1 + Math.max(left.height, right.height));
   }
 
+  private static boolean isBalancedTree2(TreeNode root) {
+    return revisionWithDFS(root).isBalance;
+  }
+
+  private static Pair revisionWithDFS(TreeNode root) {
+    if (root == null) {
+      return new Pair(true, 0);
+    }
+    Pair left = revisionWithDFS(root.left);
+    Pair right = revisionWithDFS(root.right);
+    boolean isBalanced =
+        left.isBalance && right.isBalance && Math.abs(left.height - right.height) <= 1;
+    return new Pair(isBalanced, 1 + Math.max(left.height, right.height));
+  }
+
   public static void main(String[] args) {
     //  tree 1
     TreeNode root = new TreeNode(3);
@@ -52,6 +70,7 @@ public class _11BalancedBinaryTreeE {
     root.right.left = new TreeNode(15);
     root.right.right = new TreeNode(7);
     System.out.println("yup: is root balanced tree: " + isBalanced(root));  //  true
+    System.out.println("yup: is root balanced tree: " + isBalancedTree2(root));  //  true
     //  tree 2
     TreeNode root2 = new TreeNode(1);
     root2.left = new TreeNode(2);
@@ -61,5 +80,6 @@ public class _11BalancedBinaryTreeE {
     root2.left.left.left = new TreeNode(4);
     root2.left.left.right = new TreeNode(4);
     System.out.println("yup: is root2 balanced tree: " + isBalanced(root2));  //  false
+    System.out.println("yup: is root2 balanced tree: " + isBalancedTree2(root2));  //  false
   }
 }
