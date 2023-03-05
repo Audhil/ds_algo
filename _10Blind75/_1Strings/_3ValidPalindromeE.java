9739fef7b7416ea7edc9fca9b5f6eac729c96ad1
Audhil <smdaudhilbe@gmail.com>
2023-03-05 21:01:05 +0530

    isPalindrome revision

--- _10Blind75/_1Strings/_3ValidPalindromeE.java
+++ _10Blind75/_1Strings/_3ValidPalindromeE.java
@@ -26,5 +26,27 @@ public class _3ValidPalindromeE {
   public static void main(String[] args) {
     System.out.println(
         "yup: isPlaindrome(): " + isPalindrome("A man, a plan, a canal: Panama"));  //  true
+    System.out.println(
+        "yup: isPlaindrome(): " + isPalindromeRevision("A man, a plan, a canal: Panama"));  //  true
+  }
+
+  private static boolean isPalindromeRevision(String input) {
+    int l = 0, r = input.length() - 1;
+    while (l < r) {
+      while (l < r && !Character.isLetterOrDigit(input.charAt(l))) {
+        l++;
+      }
+      while (l < r && !Character.isLetterOrDigit(input.charAt(r))) {
+        r--;
+      }
+      if (l < r) {
+        if (Character.toLowerCase(input.charAt(l)) != Character.toLowerCase(input.charAt(r))) {
+          return false;
+        }
+        l++;
+        r--;
+      }
+    }
+    return true;
   }
 }
