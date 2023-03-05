ce2aa0ef3e137f7f302ddb1563f8a1e18f4dde8f
Audhil <smdaudhilbe@gmail.com>
2023-03-05 20:47:55 +0530

    add _4TwoSumsWithBSTSpaceOptimized revision

--- _0REVISION/easy/arrays/_4TwoSumsWithBSTSpaceOptimized.java
+++ _0REVISION/easy/arrays/_4TwoSumsWithBSTSpaceOptimized.java
@@ -9,15 +9,16 @@ package _0REVISION.easy.arrays;
  */
 public class _4TwoSumsWithBSTSpaceOptimized {
 
-    public static class TreeNode {
-        public int val;
-        public TreeNode left, right;
+  public static class TreeNode {
 
-        public TreeNode(int val) {
-            this.val = val;
-            this.left = this.right = null;
-        }
+    public int val;
+    public TreeNode left, right;
+
+    public TreeNode(int val) {
+      this.val = val;
+      this.left = this.right = null;
     }
+  }
 
     /*
     *
@@ -41,39 +42,76 @@ public class _4TwoSumsWithBSTSpaceOptimized {
     }
     * */
 
-    public static boolean findTarget(TreeNode root, int k) {
-        return findValue(root, root, k);
+  public static boolean findTarget(TreeNode root, int k) {
+    return findValue(root, root, k);
+  }
+
+  private static boolean findValue(TreeNode node, TreeNode root, int k) {
+    if (root == null) {
+      return false;
     }
 
-    private static boolean findValue(TreeNode node, TreeNode root, int k) {
-        if (root == null)
-            return false;
-
-        if (find(node, k - root.val) && root.val != (k - root.val))
-            return true;
-
-        return findValue(node, root.left, k) || findValue(node, root.right, k);
+    if (find(node, k - root.val) && root.val != (k - root.val)) {
+      return true;
     }
 
-    private static boolean find(TreeNode root, int target) {
-        if (root == null)
-            return false;
+    return findValue(node, root.left, k) || findValue(node, root.right, k);
+  }
 
-        if (root.val == target)
-            return true;
-
-        if (root.val > target)
-            return find(root.left, target);
-        return find(root.right, target);
+  private static boolean find(TreeNode root, int target) {
+    if (root == null) {
+      return false;
     }
 
-    public static void main(String[] args) {
-        TreeNode root = new TreeNode(4);
-        root.left = new TreeNode(2);
-        root.right = new TreeNode(5);
-        root.left.left = new TreeNode(1);
-        root.left.right = new TreeNode(3);
-        int k = 3;
-        System.out.println("yup: findTarget: " + findTarget(root, k));
+    if (root.val == target) {
+      return true;
     }
+
+    if (root.val > target) {
+      return find(root.left, target);
+    }
+    return find(root.right, target);
+  }
+
+  public static void main(String[] args) {
+    TreeNode root = new TreeNode(4);
+    root.left = new TreeNode(2);
+    root.right = new TreeNode(5);
+    root.left.left = new TreeNode(1);
+    root.left.right = new TreeNode(3);
+    int k = 3;
+    System.out.println("yup: findTarget: " + findTarget(root, k));
+    System.out.println("yup: findTargetRevision: " + findTargetRevision(root, k));
+  }
+
+
+  //  revision
+  private static boolean findTargetRevision(TreeNode root, int k) {
+    return findValuee(root, root, k);
+  }
+
+  private static boolean findValuee(TreeNode node, TreeNode root, int k) {
+    if (root == null) {
+      return false;
+    }
+    if (findd(node, k - root.val) && (k - root.val) != root.val) {
+      return true;
+    }
+    return findValuee(root.left, node, k) || findValuee(root.right, node, k);
+  }
+
+  private static boolean findd(TreeNode root, int k) {
+    if (root == null) {
+      return false;
+    }
+
+    if (root.val == k) {
+      return true;
+    }
+
+    if (root.val > k) {
+      return findd(root.left, k);
+    }
+    return findd(root.right, k);
+  }
 }
