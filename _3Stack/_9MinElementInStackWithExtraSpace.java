ab7e61190e5cd5051baccc7cedf2fd0518705e0c
Audhil <smdaudhilbe@gmail.com>
2023-03-06 07:12:27 +0530

    add DesignMinStack prob

--- _3Stack/_9MinElementInStackWithExtraSpace.java
+++ _3Stack/_9MinElementInStackWithExtraSpace.java
@@ -3,30 +3,31 @@ package _3Stack;
 import java.util.Stack;
 
 //  https://youtu.be/asf9P2Rcopo?list=PL_z_8CaSLPWdeOezg68SKkeLN4-T_jNHd
+//  exact same explanation here: https://youtu.be/qkLl7nAwDPo - NeetCode
 public class _9MinElementInStackWithExtraSpace {
     private int[] arr = {18, 19, 29, 15, 16};
 
     private Stack<Integer> stack = new Stack<>();
-    private Stack<Integer> sStack = new Stack<>();
+    private Stack<Integer> minValueStack = new Stack<>();
 
     private void push(int item) {
         stack.push(item);
-        if (sStack.isEmpty() || sStack.peek() >= item)
-            sStack.push(item);
+        if (minValueStack.isEmpty() || minValueStack.peek() >= item)
+            minValueStack.push(item);
     }
 
     private int pop() {
         if (stack.isEmpty())
             return -1;
-        int ans = stack.peek();
-        if (sStack.peek() == ans)
-            sStack.pop();
+        int ans = stack.pop();
+        if (minValueStack.peek() == ans)
+            minValueStack.pop();
         return ans;
     }
 
     private int getMin() {
-        if (sStack.isEmpty())
+        if (minValueStack.isEmpty())
             return -1;
-        return sStack.peek();
+        return minValueStack.peek();
     }
 }
